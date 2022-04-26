38d87467cbe9091e9886cf235a967adc9c1ca219
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-04-26 16:06:37 -0500

    create working table using formdata

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const items = [
-    { name: "+5 Dexterity Vest", attributes: { sell_in: "10", quality: "20" }, },
-    { name: "Aged Brie", attributes: { sell_in: "2", quality: "0" }, },
-    { name: "Elixir of the Mongoose", attributes: { sell_in: "5", quality: "7" }, },
-    { name: "Sulfuras, Hand of Ragnaros", attributes: { sell_in: "0", quality: "80" }, },
-    { name: "Backstage passes tp a TAFKAL80ETC concert", attributes: { sell_in: "15", quality: "20" }, },
-    { name: "Conjured Mana Cake", attributes: { sell_in: "3", quality: "6" }, },
+    { name: "+5 Dexterity Vest", sell_in: "10", quality: "20" },
+    { name: "Aged Brie", sell_in: "2", quality: "0" },
+    { name: "Elixir of the Mongoose", sell_in: "5", quality: "7" },
+    { name: "Sulfuras, Hand of Ragnaros", sell_in: "0", quality: "80" },
+    { name: "Backstage passes tp a TAFKAL80ETC concert", sell_in: "15", quality: "20" },
+    { name: "Conjured Mana Cake", sell_in: "3", quality: "6" },
 ]
 
+createTable()
+addInterface()
+refreshItems()
 
-
-
-
-
-
-const $main = document.querySelector("main")
-const $app = document.querySelector("#app")
-$app.innerHTML = `
+function createTable() {
+    const $main = document.querySelector("main")
+    const $app = document.querySelector("#app")
+    $app.innerHTML = `
                 <table>
                 <thead>
                     <tr>
@@ -33,38 +32,66 @@ $app.innerHTML = `
                 </tbody>
             </table>
 `
-$main.append($app)
-addInterface()
+    $main.append($app)
+}
 
-function addItem() {
+function saveItem(item) {
+    // put the option selection in a var by getting it's NAME attribute.
+    let selectedItem = item.get("item")
+    localStorage.setItem(`${selectedItem}`, selectedItem)
+}
+
+function refreshItems() {
     const $tBody = document.querySelector("tbody")
-
-    const $tRow = document.createElement("tr")
-    $tRow.innerHTML = `
+    if (localStorage.length > 0) {
+        Object.keys(localStorage).forEach(key => {
+            items.forEach(inventoryItem => {
+                if (key === inventoryItem.name) {
+                    const $tRow = document.createElement("tr")
+                    $tRow.innerHTML = `
         <tr>
-            <td>Item Name</td>
-            <td>Item Sell In</td>
-            <td>Item Quality</td>
+            <td>${inventoryItem.name}</td>
+            <td>${inventoryItem.sell_in}</td>
+            <td>${inventoryItem.quality}</td>
         </tr>
     `
-    $tBody.append($tRow)
+                    $tBody.append($tRow)
+                }
+            })
+        })
+    }
 }
+
 
 function addInterface() {
     const $interface = document.querySelector("#interface")
     const $form = document.createElement("form")
+    $form.id = "item-selection"
     $form.innerHTML = `
             <label for="item">Select Item</label>
             <select name="item">
-                <option value="">+5 Dexterity Vest</option>
-                <option value="">Aged Brie</option>
-                <option value="">Elixir of the Mongoose</option>
-                <option value="">Sulfuras, Hand of Ragnaros</option>
-                <option value="">Backstage passes to a TAFKAL80ETC concert</option>
-                <option value="">Conjured Mana Cake</option>
-                <option value="">+5 Dexterity Vest</option>
+
             </select>
-            <input type="submit" value="Add Item">
+            <input type="submit" value="Add Item" name="selection">
     `
+    addOptions(items, $form)
     $interface.append($form)
+    addButtonListener($form)
+}
+
+function addOptions(items, $form) {
+    items.map(item => {
+        const $option = document.createElement("option")
+        $option.value = item.name
+        $option.textContent = `${item.name}`
+        $form.querySelector("select").append($option)
+    })
+}
+
+function addButtonListener($form) {
+    $form.addEventListener("submit", (event) => {
+        let formData = new FormData(event.target)
+        for (const obj of formData) { console.log(obj) }
+        saveItem(formData)
+    })
 }
