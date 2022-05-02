1d8a47cc067ec74a8e1de169734d2474de72b10d
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-05-02 14:26:53 -0500

    add styles to new form inputs

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const items = [
-    { name: "+5 Dexterity Vest", sell_in: 10, quality: 20, id: "0" },
-    { name: "Aged Brie", sell_in: 10, quality: 0, id: "1" },
-    { name: "Elixir of the Mongoose", sell_in: 10, quality: 20, id: "2" },
-    { name: "Sulfuras, Hand of Ragnaros", sell_in: 10, quality: 80, id: "3" },
-    { name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20, id: "4" },
-    { name: "Conjured Mana Cake", sell_in: 10, quality: 20, id: "5" },
-]
-
 const inventoryItems = []
 
 createTable()
@@ -20,7 +11,7 @@ function createTable() {
                 <table>
                 <thead>
                     <tr>
-                        <th colspan="4">
+                        <th colspan="5">
                         <div id="interface"></div>
                         </th>
                     </tr>
@@ -31,6 +22,7 @@ function createTable() {
                         <td>Sell In</td>
                         <td>Quality</td>
                         <td>Date of Entry</td>
+                        <td>Category</td>
                     </tr>
                 </tbody>
             </table>
@@ -43,13 +35,27 @@ function addItemToList(item) {
     let selectedDate = item.get("date")
     let selectedQuality = item.get("quality")
     let selectedSell_in = item.get("sell_in")
-    const inventoryItem = {
-        name: selectedItem,
-        sell_in: selectedSell_in,
-        quality: selectedQuality,
-        date: selectedDate,
+    let selectedCategory = createCategory(selectedItem)
+    if (selectedCategory === "legendary") {
+        const inventoryItem = {
+            name: selectedItem,
+            sell_in: selectedSell_in,
+            quality: 80,
+            date_added: selectedDate,
+            category: selectedCategory
+        }
+        inventoryItems.push(inventoryItem)
+
+    } else {
+        const inventoryItem = {
+            name: selectedItem,
+            sell_in: selectedSell_in,
+            quality: selectedQuality,
+            date_added: selectedDate,
+            category: selectedCategory
+        }
+        inventoryItems.push(inventoryItem)
     }
-    inventoryItems.push(inventoryItem)
 }
 
 function saveItem(item) {
@@ -83,7 +89,8 @@ function refreshItems() {
             <td>${savedItem.name}</td>
             <td>${savedItem.sell_in}</td>
             <td>${savedItem.quality}</td>
-            <td>${savedItem.date}</td>
+            <td>${savedItem.date_added}</td>
+            <td>${savedItem.category}</td>
         </tr>
     `
             $tBody.append($tRow)
@@ -93,83 +100,62 @@ function refreshItems() {
 
 
 function addInterface() {
+    const $app = document.querySelector("#app")
     const $interface = document.querySelector("#interface")
-    const $form = document.createElement("form")
-    $form.id = "item-selection"
-    $form.innerHTML = `
+    const $form1 = document.createElement("form")
+    $form1.id = "form-1"
+    $form1.innerHTML = `
             <label for="item">Enter Item</label>
-            <input type="text" name="item" required>
+            <input type="text" name="item" value="conjured vest" required>
             <label for="date">Date of Entry</label>
-            <input type="number" name="date" required>
+            <input type="date" name="date" required>
             <label for="quality">Quality</label>
             <input type="number" name="quality" required>
             <label for="sell_in">Sell-in Days</label>
             <input type="number" name="sell_in" required>
             <input type="submit" value="Add Item" name="item date quality sell_in">
             <input type="reset" value="Delete Inventory">
-            <input type="button" value="EOD" id="eod">
     `
-    $interface.append($form)
-    addListeners($form)
+    $interface.append($form1)
+    const $form2 = document.createElement("form")
+    $form2.id = "form-2"
+    $form2.innerHTML = `
+        <label for="current-date">Date of Degradation</label>
+        <input type="date" name="current-date" required>
+        <input type="submit" value="Apply Degradation" name="current-date">
+    `
+    $app.append($form2)
+    addListeners($form1, $form2)
 }
 
-function addListeners($form) {
-    $form.addEventListener("submit", (event) => {
+function addListeners($form1, $form2) {
+    $form1.addEventListener("submit", (event) => {
         let formData = new FormData(event.currentTarget)
         saveItem(formData)
     })
-    $form.addEventListener("reset", (event) => {
+    $form1.addEventListener("reset", (event) => {
         localStorage.clear()
         window.location.reload()
     })
-    $form.querySelector("#eod").addEventListener("click", (event) => {
-        eodCalculate()
-        window.location.reload()
+    $form2.addEventListener("submit", (event) => {
+        let formData = new FormData(event.currentTarget)
+        eodCalculate(formData)
     })
 }
 
-function eodCalculate() {
+function eodCalculate(date) {
+    let currentDate = date.get("current-date")
     const savedInventory = localStorage.getItem("items")
     const parsedInventory = JSON.parse(savedInventory)
     let category = ""
     parsedInventory.forEach(item => {
-        if (item.name.includes("conjured")) {
-
-        } else if (item.name.includes("sulfuras")) {
-
-        } else if (item.name.includes("aged brie")) {
-
-        } else if (item.name.includes("backstage passes")) {
-
-        } else {
-
+        let days = getTimeInDays(item.date_added, currentDate)
+        for (let i = 0; i < days; i++) {
+            reduceItem(item)
         }
-
-        switch (category) {
-            case ("conjured"):
-                console.log("got it")
-
-                break;
-            case "1":
-
-                break;
-            case "2":
-
-                break;
-            case "3":
-                break;
-            case "4":
-
-                break;
-            case "5":
-
-                break;
-            default:
-                break;
-        }
-        eodUpdateLocalStorage(parsedInventory)
-        refreshItems()
     })
+    eodUpdateLocalStorage(parsedInventory)
+    refreshItems()
 }
 
 function eodUpdateLocalStorage(inventory) {
@@ -177,8 +163,88 @@ function eodUpdateLocalStorage(inventory) {
     localStorage.setItem("items", itemsJSON)
 }
 
+function createCategory(item) {
+    let category = ""
+    if (item.includes("conjured")) {
+        category = "conjured"
+    } else if (item.includes("sulfuras")) {
+        category = "legendary"
+    } else if (item.includes("aged brie")) {
+        category = "aged brie"
+    } else if (item.includes("backstage passes")) {
+        category = "backstage passes"
+    } else {
+        category = "standard"
+    }
+    return category
+}
 
+function reduceItem(item) {
+    switch (item.category) {
+        case "legendary":
+            break;
+        case "aged brie":
+            item.sell_in = item.sell_in - 1
+            item.quality = item.quality + 1
+            if (item.quality > 50) {
+                item.quality = 50
+            } else if (item.quality < 0) {
+                item.quality = 0
+            }
+            break;
+        case "backstage passes":
+            item.sell_in = item.sell_in - 1
+            if (item.sell_in <= 10 && item.sell_in > 5) {
+                item.quality = item.quality + 2
+            } else if (item.sell_in <= 5 && item.sell_in >= 0) {
+                item.quality = item.quality + 3
+            } else if (item.sell_in < 0) {
+                item.quality = 0
+            } else {
+                item.quality = item.quality + 1
+            }
+            if (item.quality > 50) {
+                item.quality = 50
+            } else if (item.quality < 0) {
+                item.quality = 0
+            }
+            break;
+        case "conjured":
+            item.sell_in = item.sell_in - 1
+            if (item.sell_in < 0) {
+                item.quality = item.quality - 4
+            } else {
+                item.quality = item.quality - 2
+            }
+            if (item.quality > 50) {
+                item.quality = 50
+            } else if (item.quality < 0) {
+                item.quality = 0
+            }
+            break;
+        default:
+            item.sell_in = item.sell_in - 1
+            if (item.sell_in < 0) {
+                item.quality = item.quality - 2
+            } else {
+                item.quality = item.quality - 1
+            }
+            if (item.quality > 50) {
+                item.quality = 50
+            } else if (item.quality < 0) {
+                item.quality = 0
+            }
+            break;
+    }
+}
 
+function getTimeInDays(dateEntered, currentDate) {
+    let date1 = new Date(dateEntered)
+    let date2 = new Date(currentDate)
+    let differenceInTime = date2.getTime() - date1.getTime()
+    let differenceInDays = differenceInTime / (1000 * 3600 * 24);
+    return differenceInDays
+}
 
 
 /*
